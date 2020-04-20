fe5f023b37e420b86956d5c2e4cf38fda70f2daa
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 13:20:35 -0600

    styling bio

--- client/src/components/bio/Bio.js
+++ client/src/components/bio/Bio.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import profilePic from './profileMonopoly.jpeg'
+import './Bio.css'
 
 const Bio = () => {
 	return (
-		<Jumbotron style={{ background: '#00D2A3', margin: '1rem', padding: '10px' }}>
+		<Jumbotron style={{ background: '#323C39', margin: '1rem' }}>
 			<center>
 				<img src={profilePic} height="500" />
 			</center>
-			<p>
-				Hello! Welcome to my portfolio site! My Name is Freddy Silber and I am a full stack web developer.
-				I was born in a small mountain town called Fraser in the state of Colorado. I'm a snowboarder but that doesn't pay the bills so now I'm a developer.
+			<div className="bio">
+				<p className="bioText">
+					Hello! Welcome to my portfolio site! My Name is Freddy Silber and I am a full stack web developer.
+					I was born in a small mountain town called Fraser in the state of Colorado. I'm a snowboarder but that doesn't pay the bills so now I'm a developer.
 			</p>
+			</div>
 		</Jumbotron>
 	)
 }
