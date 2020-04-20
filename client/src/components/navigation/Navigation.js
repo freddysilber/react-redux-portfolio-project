f1716db2064d0434c0d5c8d5d721e51e320ad0cf
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 12:59:24 -0600

    coloring app from brand colors

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -5,10 +5,10 @@ import './Navigation.css'
 const Navigation = () => {
 	return (
 		<div className="navbar navigation">
-			<NavLink exact to='/' activeStyle={{ color: 'lightgreen', fontWeight: 'bold' }}>About Me</NavLink>
-			<NavLink to='/resume' activeStyle={{ color: 'lightpink', fontWeight: 'bold' }}>Resume</NavLink>
-			<NavLink to='/portfolioprojects' activeStyle={{ color: 'lightskyblue', fontWeight: 'bold' }}>Portfolio Projects</NavLink>
-			<NavLink to='/covid19' activeStyle={{ color: 'gold', fontWeight: 'bold' }}>Covid 19</NavLink>
+			<NavLink exact to='/' className="navLink" activeStyle={{ color: '#905100', fontWeight: 'bold' }}>About Me</NavLink>
+			<NavLink to='/resume' className="navLink" activeStyle={{ color: '#005D48', fontWeight: 'bold' }}>Resume</NavLink>
+			<NavLink to='/portfolioprojects' className="navLink" activeStyle={{ color: '#FF6200', fontWeight: 'bold' }}>Portfolio Projects</NavLink>
+			<NavLink to='/covid19' className="navLink" activeStyle={{ color: '#0A7FD2', fontWeight: 'bold' }}>Covid 19</NavLink>
 		</div>
 	)
 }
