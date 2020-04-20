96be29f1c7503407046318f97c4a3afb5f01b927
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 13:16:44 -0600

    add image to bio page

--- client/src/components/bio/Bio.js
+++ client/src/components/bio/Bio.js
@@ -1,12 +1,13 @@
-import React from './node_modules/react'
-import Jumbotron from './node_modules/react-bootstrap/Jumbotron'
-// import profilePic from './profileMonopoly.jpeg'
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import profilePic from './profileMonopoly.jpeg'
 
 const Bio = () => {
 	return (
-		<Jumbotron style={{ background: '#00D2A3', margin: '1rem' }}>
-			<h1>Bio</h1>
-			{/* <img src={profilePic} /> */}
+		<Jumbotron style={{ background: '#00D2A3', margin: '1rem', padding: '10px' }}>
+			<center>
+				<img src={profilePic} height="500" />
+			</center>
 			<p>
 				Hello! Welcome to my portfolio site! My Name is Freddy Silber and I am a full stack web developer.
 				I was born in a small mountain town called Fraser in the state of Colorado. I'm a snowboarder but that doesn't pay the bills so now I'm a developer.
