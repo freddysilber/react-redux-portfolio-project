4c6236de4d4fcf9e4f9aa162f443667c912ae1f8
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 14:08:43 -0600

    using bootstrap image

--- client/src/components/bio/Bio.js
+++ client/src/components/bio/Bio.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 import profilePic from './profileMonopoly.jpeg'
 import './Bio.css'
 
 const Bio = () => {
 	return (
-		<Jumbotron style={{ background: '#323C39', margin: '1rem' }}>
-			<center>
-				<img src={profilePic} height="500" />
-			</center>
-			<div className="bio">
-				<p className="bioText">
-					Hello! Welcome to my portfolio site! My Name is Freddy Silber and I am a full stack web developer.
-					I was born in a small mountain town called Fraser in the state of Colorado. I'm a snowboarder but that doesn't pay the bills so now I'm a developer.
-			</p>
+		<Jumbotron fluid style={{ background: '#323C39', margin: '1rem', padding: '2rem' }}>
+			<div className="content">
+				<Col xs={6} md={4}>
+					<Image src={profilePic} roundedCircle height="400" />
+				</Col>
+				<div className="bio">
+					<p className="bioText">
+						Hello! Welcome to my portfolio site! My Name is Freddy Silber and I am a full stack web developer.
+						I was born in a small mountain town called Fraser in the state of Colorado. I'm a snowboarder but that doesn't pay the bills so now I'm a developer.
+					</p>
+				</div>
 			</div>
 		</Jumbotron>
 	)
--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Counter from '../components/counter/Counter'
-// import Bio from '../components/bio/Bio'
 import Bio from '../components/bio/Bio'
 
 export default class HomeContainer extends React.Component {
 	render() {
 		return (
 			<div>
+				<h1>Welcome!</h1>
 				<Bio />
 				<Counter />
 			</div>
