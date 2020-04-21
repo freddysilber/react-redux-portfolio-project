2a4aab74e20d634fcfe2fa7467852fcfd34dac15
freddysilber <freddy.silber@cloudcoach.com>
2020-04-21 11:05:19 -0600

    refactoring infoContainer

--- client/src/containers/InfoContainer.js
+++ client/src/containers/InfoContainer.js
@@ -8,18 +8,19 @@ export default class InfoContainer extends React.Component {
 	render() {
 		return (
 			<div>
-				<Tabs defaultActiveKey="message" id="uncontrolled-tab-example">
-					<Tab eventKey="message" title="Send me a message">
+				<Tabs defaultActiveKey="comment">
+					<Tab eventKey="comment" title="Leave a comment">
 						<CommentForm />
 					</Tab>
-					<Tab eventKey="profile" title="Contact Info">
+					<Tab eventKey="allComments" title="All Comments">
+						<p>show all comments here</p>
+					</Tab>
+					<Tab eventKey="contact" title="Contact Me">
 						<div>
 							<p>Personal Email: {contactInfo.personalEmail}</p>
 							<p>Work Email: {contactInfo.workEmail}</p>
 							<p>Phone: {contactInfo.phone}</p>
 						</div>
-					</Tab>
-					<Tab eventKey="contact" title="Contact" disabled>
 					</Tab>
 				</Tabs>
 			</div>
