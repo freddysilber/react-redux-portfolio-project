ac4de0455c889e757b3e1b32bf6aeeffbbe1e55c
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 18:23:38 -0600

    updated nav colors

--- client/src/components/navigation/Navigation.js
+++ client/src/components/navigation/Navigation.js
@@ -5,10 +5,10 @@ import './Navigation.css'
 const Navigation = () => {
 	return (
 		<div className="navbar navigation">
-			<NavLink exact to='/' className="navLink" activeStyle={{ color: '#905100', fontWeight: 'bold' }}>About Me</NavLink>
-			<NavLink to='/resume' className="navLink" activeStyle={{ color: '#005D48', fontWeight: 'bold' }}>Resume</NavLink>
-			<NavLink to='/portfolioprojects' className="navLink" activeStyle={{ color: '#FF6200', fontWeight: 'bold' }}>Portfolio Projects</NavLink>
-			<NavLink to='/covid19' className="navLink" activeStyle={{ color: '#0A7FD2', fontWeight: 'bold' }}>Covid 19</NavLink>
+			<NavLink exact to='/' className="navLink" activeStyle={{ color: '#90BDDC', fontWeight: 'bold' }}>About Me</NavLink>
+			<NavLink to='/resume' className="navLink" activeStyle={{ color: '#90BDDC', fontWeight: 'bold' }}>Resume</NavLink>
+			<NavLink to='/portfolioprojects' className="navLink" activeStyle={{ color: '#90BDDC', fontWeight: 'bold' }}>Portfolio Projects</NavLink>
+			<NavLink to='/covid19' className="navLink" activeStyle={{ color: '#90BDDC', fontWeight: 'bold' }}>Covid 19</NavLink>
 		</div>
 	)
 }
