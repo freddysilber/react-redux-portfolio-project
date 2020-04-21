a9242ace6be561f10bf6f8b1be067c6654da69db
freddysilber <freddy.silber@cloudcoach.com>
2020-04-21 10:49:43 -0600

    stubbing out a basic form

--- client/src/containers/InfoContainer.js
+++ client/src/containers/InfoContainer.js
@@ -1,15 +1,45 @@
 import React from 'react'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 import { contactInfo } from '../data/util'
 
 export default class InfoContainer extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			message: ''
+		}
+	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		console.log('NEED TO SUBMIT THIS MSG', this.state.message)
+	}
+
 	render() {
 		return (
 			<div>
-				<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-					<Tab eventKey="home" title="Send me a message">
-						<p>send me a message!</p>
+				<Tabs defaultActiveKey="message" id="uncontrolled-tab-example">
+					<Tab eventKey="message" title="Send me a message">
+						<form onSubmit={this.handleSubmit}>
+							<InputGroup>
+								<InputGroup.Prepend>
+									<InputGroup.Text>Leave a Message!</InputGroup.Text>
+								</InputGroup.Prepend>
+								<FormControl as="textarea" aria-label="message" name="message" onChange={this.handleChange} />
+							</InputGroup>
+							<br />
+							<Button type="submit" variant="outline-success">Submit</Button>
+						</form>
 					</Tab>
 					<Tab eventKey="profile" title="Contact Info">
 						<div>
