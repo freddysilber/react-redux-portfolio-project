84069d4d5be1ea9297f4e6d692d7b223765e755d
freddysilber <freddy.silber@cloudcoach.com>
2020-04-21 09:01:38 -0600

    font for carousel

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -8,7 +8,7 @@ import crestedButte from '../pictures/crestedButte.jpeg'
 export default class CustomCarousel extends React.Component {
 	render() {
 		return (
-			<div style={{ background: 'lightgray' }}>
+			<div className="carouselContainer">
 				<Carousel>
 					<Carousel.Item>
 						<img className="d-block w-100" height="300" src={sunset} />
