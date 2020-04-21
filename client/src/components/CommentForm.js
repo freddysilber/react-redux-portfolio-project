59e74ac8305fc2b14dc9631a1ff9a5a045b437bf
freddysilber <freddy.silber@cloudcoach.com>
2020-04-21 11:02:10 -0600

    building comment form

---
+++ client/src/components/CommentForm.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+export default class CommentForm extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			firstName: '',
+			lastName: '',
+			email: '',
+			comment: ''
+		}
+	}
+
+	handleChange = event => {
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
+	handleSubmit = event => {
+		event.preventDefault()
+		console.log('NEED TO SUBMIT THIS MSG', this.state)
+	}
+
+	render() {
+		return (
+			<Form onSubmit={this.handleSubmit}>
+				<Form.Group controlId="emailAddress">
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type="email" placeholder="Enter email" name="email" onChange={this.handleChange} />
+				</Form.Group>
+
+				<Form.Group controlId="firstName">
+					<Form.Label>First Name</Form.Label>
+					<Form.Control type="text" placeholder="First Name" name="firstName" onChange={this.handleChange} />
+				</Form.Group>
+
+				<Form.Group controlId="lastName">
+					<Form.Label>First Name</Form.Label>
+					<Form.Control type="text" placeholder="Last Name" name="lastName" onChange={this.handleChange} />
+				</Form.Group>
+
+				<Form.Group controlId="comment">
+					<Form.Label>Comment</Form.Label>
+					<FormControl as="textarea" aria-label="comment" name="comment" onChange={this.handleChange} />
+				</Form.Group>
+
+				<Button variant="primary" type="submit">Submit</Button>
+			</Form>
+		)
+	}
+}
--- client/src/containers/InfoContainer.js
+++ client/src/containers/InfoContainer.js
@@ -1,45 +1,16 @@
 import React from 'react'
+import CommentForm from '../components/CommentForm'
 import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
-import InputGroup from 'react-bootstrap/InputGroup'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
 import { contactInfo } from '../data/util'
 
 export default class InfoContainer extends React.Component {
-	constructor() {
-		super()
-		this.state = {
-			message: ''
-		}
-	}
-
-	handleChange = event => {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
-
-	handleSubmit = event => {
-		event.preventDefault()
-		console.log('NEED TO SUBMIT THIS MSG', this.state.message)
-	}
-
 	render() {
 		return (
 			<div>
 				<Tabs defaultActiveKey="message" id="uncontrolled-tab-example">
 					<Tab eventKey="message" title="Send me a message">
-						<form onSubmit={this.handleSubmit}>
-							<InputGroup>
-								<InputGroup.Prepend>
-									<InputGroup.Text>Leave a Message!</InputGroup.Text>
-								</InputGroup.Prepend>
-								<FormControl as="textarea" aria-label="message" name="message" onChange={this.handleChange} />
-							</InputGroup>
-							<br />
-							<Button type="submit" variant="outline-success">Submit</Button>
-						</form>
+						<CommentForm />
 					</Tab>
 					<Tab eventKey="profile" title="Contact Info">
 						<div>
