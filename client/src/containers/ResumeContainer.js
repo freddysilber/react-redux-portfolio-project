526d75a0ad73403d96fbf9c235e652aba9d92d2c
freddysilber <freddy.silber@cloudcoach.com>
2020-04-20 22:28:17 -0600

    filled in resume container with temp jobs

--- client/src/containers/ResumeContainer.js
+++ client/src/containers/ResumeContainer.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import Spinner from 'react-bootstrap/Spinner'
-import Counter from '../components/Counter'
+// import Counter from '../components/Counter'
+import Card from 'react-bootstrap/Card'
 
-export default class ResumeContainer extends React.Component {
-	render() {
-		return (
-			<div className="text-center">
-				{/* <div style={{ top: '50%', left: '50%', position: 'fixed' }}> */}
-				<Spinner animation="border" variant="dark" />
+const ResumeContainer = () => {
+	return (
+		<div className="resumeContentContainer">
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>Fraser Valley Hot Dog</Card.Title>
+					<Card.Text>This was my first job in high school. I worked as a line cook at a small hotdog and burger shop.</Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>The Foundry</Card.Title>
+					<Card.Text>Worked here on and off through high school doing almost everything from back of the house to front of the house. </Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>Pole Creek (Bistro 28)</Card.Title>
+					<Card.Text>My first full time job as a chef at a golf course.</Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>Snow Mountain Builders</Card.Title>
+					<Card.Text>Worked for my dad for a summer doing construction.</Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>Solstice Winter Bistro</Card.Title>
+					<Card.Text>My Second full time job as a chef creating fancy small plates at a local cocktail lounge.</Card.Text>
+				</Card.Body>
+			</Card>
+			<Card className="cardItem">
+				<Card.Body>
+					<Card.Title>Cloud Coach</Card.Title>
+					<Card.Text>Working currently as a developer doing project management software on Salesforce. #SAS.</Card.Text>
+				</Card.Body>
+			</Card>
+			{/* <div className="text-center">
 				<Counter />
-			</div>
-		)
-	}
+			</div> */}
+		</div>
+	)
 }
+
+export default ResumeContainer
