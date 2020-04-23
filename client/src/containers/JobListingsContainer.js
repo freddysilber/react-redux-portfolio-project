71c049d4960175317f6e27768c301d4210057692
freddysilber <freddy.silber@cloudcoach.com>
2020-04-22 21:12:19 -0600

    stubbing out joblistings container

--- client/src/containers/JobListingsContainer.js
+++ client/src/containers/JobListingsContainer.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+import { connect } from 'react-redux'
+import { getJobListings } from '../actions/joblistingActions'
 
-export default class JobListingsContainer extends React.Component {
+class JobListingsContainer extends React.Component {
+	componentDidMount() {
+		this.props.getJobListings()
+	}
+
 	render() {
-		return (
-			<div>JOB LISTINGS CONTAINER</div>
-		)
+		return this.handleLoading()
+	}
+
+	handleLoading = () => {
+		if (this.props.loading) {
+			return (
+				<div className="spinnerContainer">
+					<Spinner animation="border" variant="dark" />
+				</div >
+			)
+		} else {
+			return (
+				<>
+					<p>thing</p>
+				</>
+			)
+		}
 	}
 }
+
+const mapStateToProps = state => {
+	return {
+		joblistings: state.joblistings.joblistings,
+		loading: state.joblistings.loading
+	}
+}
+
+export default connect(mapStateToProps, { getJobListings })(JobListingsContainer)
