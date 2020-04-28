a6dfc66b6b77576356a9e4b4d6f98fd20b29bf86
freddysilber <freddy.silber@cloudcoach.com>
2020-04-28 11:37:11 -0600

    comments for user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
 	has_secure_password
-  
+	# validate that users dont have the same email
 	validates :email, presence: true, uniqueness: true
-	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+	# validate that user's email is in the correct format
+	# validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+	# validate that users dont have the same username
 	validates :username, presence: true, uniqueness: true
-	validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+	# validate that a user's password is at least 6 characters 
+	# validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
   end
   
