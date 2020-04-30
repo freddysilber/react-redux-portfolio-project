713965111636cb280ea7d871e23812e858783970
freddysilber <freddy.silber@cloudcoach.com>
2020-04-29 18:06:48 -0600

    importing font awesome icon npm packages

--- client/src/components/SocialMediaIcons.js
+++ client/src/components/SocialMediaIcons.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { socialMediaUrls } from '../data/util'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const SocialMediaIcons = () => {
 	return (
 		<ul className="socialMediaIconList">
+			{/* <FontAwesomeIcon icon={faTwitter} href={socialMediaUrls.twitter} /> */}
+			{/* <a href={socialMediaUrls.twitter} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className="fab fa-twitter fa-lg twitterIcon" icon={faTwitter} /></a> */}
 			<a href={socialMediaUrls.twitter} target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter fa-lg twitterIcon"></i></a>
 			<a href={socialMediaUrls.facebook} target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook fa-lg facebookIcon"></i></a>
 			<a href={socialMediaUrls.instagram} target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram fa-lg instagramIcon"></i></a>
