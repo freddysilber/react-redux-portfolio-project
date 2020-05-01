71d8f7294da2730441653caf78c387824a6388c9
freddysilber <freddy.silber@cloudcoach.com>
2020-04-30 19:02:35 -0600

    updates active color for nav icons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -7,12 +7,12 @@ import { faHome, faVirus, faCogs, faBriefcase, faBlog } from '@fortawesome/free-
 const Navigation = () => {
 	return (
 		<div className="navbar navigation">
-			<NavLink exact to='/' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faHome} /></NavLink>
-			<NavLink to='/resume' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faBriefcase} /></NavLink>
-			<NavLink to='/portfolioprojects' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faGithub} /></NavLink>
-			<NavLink to='/covid19' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faVirus} /></NavLink>
-			<NavLink to='/blog' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faBlog} /></NavLink>
-			<NavLink to='/admin' className="navLink" activeStyle={{ color: '#FFD6A1', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faCogs} /></NavLink>
+			<NavLink exact to='/' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faHome} /></NavLink>
+			<NavLink to='/resume' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faBriefcase} /></NavLink>
+			<NavLink to='/portfolioprojects' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faGithub} /></NavLink>
+			<NavLink to='/covid19' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faVirus} /></NavLink>
+			<NavLink to='/blog' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faBlog} /></NavLink>
+			<NavLink to='/admin' className="navLink" activeStyle={{ color: '#00D2A9', fontWeight: 'bold', textDecoration: 'underline' }}><FontAwesomeIcon icon={faCogs} /></NavLink>
 		</div>
 	)
 }
