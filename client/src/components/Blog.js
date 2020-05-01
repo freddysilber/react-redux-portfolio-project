45cb6c76c67681541b2f8e109d20e0596e671e7c
freddysilber <freddy.silber@cloudcoach.com>
2020-04-30 19:16:16 -0600

    refactored blog to functional cmp

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-export default class Blog extends React.Component {
-	render() {
-		return <iframe src="https://freddysilber.github.io/" width="100%" height="700px" />
-	}
-}
+const Blog = () => <iframe src="https://freddysilber.github.io/" width="100%" height="750px" />
+
+export default Blog
