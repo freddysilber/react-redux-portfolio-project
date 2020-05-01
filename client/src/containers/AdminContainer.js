3f42742a444b0bc5d7c288b2a275905b22cab828
freddysilber <freddy.silber@cloudcoach.com>
2020-04-30 19:06:41 -0600

    adds icons to admin container

--- client/src/containers/AdminContainer.js
+++ client/src/containers/AdminContainer.js
@@ -2,19 +2,21 @@ import React from 'react'
 import UsersContainer from './UsersContainer'
 import JobListingsContainer from './JobListingsContainer'
 import { Tab, Row, Col, Nav } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBriefcase, faUsers } from '@fortawesome/free-solid-svg-icons'
 
 export default class AdminContainer extends React.Component {
 	render() {
 		return (
 			<Tab.Container defaultActiveKey="users">
 				<Row>
-					<Col xs={2}>
+					<Col xs={1}>
 						<Nav variant="pills" className="flex-column">
 							<Nav.Item>
-								<Nav.Link eventKey="users">Users</Nav.Link>
+								<Nav.Link eventKey="users"><FontAwesomeIcon icon={faUsers} /></Nav.Link>
 							</Nav.Item>
 							<Nav.Item>
-								<Nav.Link eventKey="jobListings">Job Listings (Resume)</Nav.Link>
+								<Nav.Link eventKey="jobListings"><FontAwesomeIcon icon={faBriefcase} /></Nav.Link>
 							</Nav.Item>
 						</Nav>
 					</Col>
