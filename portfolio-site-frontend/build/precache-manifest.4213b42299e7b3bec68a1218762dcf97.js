0e8f6ad1b35abd2a2ef571fea1dd2e3a7e6db1dd
freddysilber <freddy.silber@cloudcoach.com>
2020-05-08 18:29:35 -0600

    project stucture

---
+++ portfolio-site-frontend/build/precache-manifest.4213b42299e7b3bec68a1218762dcf97.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4374d851589935dfd41ea3934ab2ad31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78333f581b390957d891",
+    "url": "/static/css/2.9e7b09f4.chunk.css"
+  },
+  {
+    "revision": "c356882ad7b74213cd50",
+    "url": "/static/css/main.a48ca8ed.chunk.css"
+  },
+  {
+    "revision": "78333f581b390957d891",
+    "url": "/static/js/2.12b2656a.chunk.js"
+  },
+  {
+    "revision": "1f343f3b00bd42841cd23cdb6e2b08ec",
+    "url": "/static/js/2.12b2656a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c356882ad7b74213cd50",
+    "url": "/static/js/main.193cce20.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "05aa77b14a3e3c5a79b60b00b34e9652",
+    "url": "/static/media/BalooTamma2-Regular.05aa77b1.ttf"
+  },
+  {
+    "revision": "53d5432732367b2eb8f30219665db188",
+    "url": "/static/media/DramaSans.53d54327.ttf"
+  },
+  {
+    "revision": "a5d23e1ac49a233f0fc77710bab69cf9",
+    "url": "/static/media/cloudcoach.a5d23e1a.jpg"
+  },
+  {
+    "revision": "7efe77f5b70ef099c0029102eac5c4df",
+    "url": "/static/media/crestedButte.7efe77f5.jpeg"
+  },
+  {
+    "revision": "a3a728cc0d1dcb757ac73ff6b5563ae0",
+    "url": "/static/media/londonTower.a3a728cc.jpeg"
+  },
+  {
+    "revision": "5781ffbcb0e4203a1cb14a2cd59b84a0",
+    "url": "/static/media/squareTopLakes.5781ffbc.jpeg"
+  },
+  {
+    "revision": "e57a824a03004615d2f9f73a99214232",
+    "url": "/static/media/sunset.e57a824a.jpeg"
+  }
+]);
