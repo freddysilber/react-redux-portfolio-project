d9d5715417985058c5a1139038743fd87240aa9a
freddysilber <freddy.silber@cloudcoach.com>
2020-05-09 18:18:45 -0600

    adds weekly banner

--- portfolio-site-frontend/src/components/WeeklyBanner.js
+++ portfolio-site-frontend/src/components/WeeklyBanner.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Alert } from 'react-bootstrap'
 
 export default class WeeklyBanner extends React.Component {
 	render() {
 		return (
-			<center style={{ background: 'lightgray', color: 'black', padding: '1rem' }}>
-				<h1>This Weeks Highlight: <a href="https://freddysilber.github.io/">My Journey as a Software Engineer</a></h1>
-			</center>
+			<Alert variant="dark" style={{ marginBottom: '0' }}>
+				<Alert.Heading>This Week's Highlight</Alert.Heading>
+				<a href="https://freddysilber.github.io/">My Journey as a Software Engineer</a>
+			</Alert>
 		)
 	}
 }
