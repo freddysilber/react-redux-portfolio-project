0271f72194cbb21d5076b320e68c31e60b9e2565
freddysilber <freddy.silber@gmail.com>
2020-05-12 16:54:27 -0600

    adds text to nav

--- portfolio-site-frontend/src/components/Navigation.js
+++ portfolio-site-frontend/src/components/Navigation.js
@@ -8,23 +8,23 @@ const Navigation = () => {
 	return (
 		<Nav fill variant="pills" defaultActiveKey="/" activeKey={window.location.pathname}>
 			<Nav.Item>
-				<Nav.Link href="/" className="navLink"><FontAwesomeIcon icon={faHome} /></Nav.Link>
+				<Nav.Link href="/" className="navLink"><FontAwesomeIcon icon={faHome} /> Home</Nav.Link>
+			</Nav.Item>
+			{/* <Nav.Item>
+				<Nav.Link href="/resume" className="navLink"><FontAwesomeIcon icon={faBriefcase} /> Resume</Nav.Link>
+			</Nav.Item> */}
+			<Nav.Item>
+				<Nav.Link href="/portfolioprojects" className="navLink"><FontAwesomeIcon icon={faGithub} /> Projects</Nav.Link>
 			</Nav.Item>
 			<Nav.Item>
-				<Nav.Link href="/resume" className="navLink"><FontAwesomeIcon icon={faBriefcase} /></Nav.Link>
+				<Nav.Link href="/covid19" className="navLink"><FontAwesomeIcon icon={faVirus} /> Covid19</Nav.Link>
 			</Nav.Item>
 			<Nav.Item>
-				<Nav.Link href="/portfolioprojects" className="navLink"><FontAwesomeIcon icon={faGithub} /></Nav.Link>
+				<Nav.Link href="/blog" className="navLink"><FontAwesomeIcon icon={faBlog} /> Blog</Nav.Link>
 			</Nav.Item>
-			<Nav.Item>
-				<Nav.Link href="/covid19" className="navLink"><FontAwesomeIcon icon={faVirus} /></Nav.Link>
-			</Nav.Item>
-			<Nav.Item>
-				<Nav.Link href="/blog" className="navLink"><FontAwesomeIcon icon={faBlog} /></Nav.Link>
-			</Nav.Item>
-			<Nav.Item>
+			{/* <Nav.Item>
 				<Nav.Link href="/admin" className="navLink"><FontAwesomeIcon icon={faCogs} /></Nav.Link>
-			</Nav.Item>
+			</Nav.Item> */}
 		</Nav>
 	)
 }
