10b5324f3bc9393526d49323abc667407980d1a7
freddysilber <freddy.silber@gmail.com>
2020-05-12 16:52:24 -0600

    adds text to header

--- portfolio-site-frontend/src/components/Header.js
+++ portfolio-site-frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 	return (
 		<div className="headerContainer">
-			<h1><em>Freddy Silber</em></h1>
+			<h1><em>Freddy Silber | Software Engineer</em></h1>
 		</div>
 	)
 }
