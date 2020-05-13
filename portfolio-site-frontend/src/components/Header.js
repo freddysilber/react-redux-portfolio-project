fc37c00302590342deb5194af8ff07ab43596e51
freddysilber <freddy.silber@gmail.com>
2020-05-13 10:09:17 -0600

    adds more skill icons

--- portfolio-site-frontend/src/components/Header.js
+++ portfolio-site-frontend/src/components/Header.js
@@ -5,12 +5,14 @@ const Header = () => {
 	return (
 		<div className="headerContainer">
 			<h1><em>Freddy Silber | Software Engineer</em></h1>
+			<br />
 			<center>
 				<i class="devicon-ruby-plain"></i>
 				<i class="devicon-javascript-plain"></i>
 				<i class="devicon-react-original"></i>
 				<i class="devicon-babel-plain"></i>
 				<i class="devicon-css3-plain"></i>
+				<i class="devicon-bootstrap-plain"></i>
 				<i class="devicon-git-plain"></i>
 				<i class="devicon-github-plain"></i>
 				<i class="devicon-html5-plain"></i>
@@ -20,7 +22,13 @@ const Header = () => {
 				<i class="devicon-sass-original"></i>
 				<i class="devicon-webpack-plain"></i>
 				<i class="devicon-java-plain"></i>
+				<i class="devicon-d3js-plain"></i>
+				<i class="devicon-postgresql-plain"></i>
+				<i class="devicon-typescript-plain"></i>
+				<i class="devicon-visualstudio-plain"></i>
+				<i class="devicon-wordpress-plain"></i>
 			</center>
+			<br />
 		</div>
 	)
 }
